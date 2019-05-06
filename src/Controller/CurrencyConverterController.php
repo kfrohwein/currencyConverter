3a66896a1848c78b360bf9439bcc2a797b0affd9
Karsten Frohwein <karsten.frohwein@4fb.de>
2019-05-06 21:23:43 +0200

    Added conversion route

--- src/Controller/CurrencyConverterController.php
+++ src/Controller/CurrencyConverterController.php
@@ -2,11 +2,22 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+
 class CurrencyConverterController
 {
-    public function number()
+    /**
+     * Simple controller to handle a route that contains currencies and an amount to be converted.
+     * The route validates the desired input for use.
+     * @param string $currencySource
+     * @param string $currencyTarget
+     * @param string $amount
+     * @return JsonResponse
+     */
+    public function convert(string $currencySource, string $currencyTarget, string $amount): JsonResponse
     {
 
+        return new JsonResponse(['data' => 123]);
 
     }
 }
